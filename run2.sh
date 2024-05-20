68409c087478065e810f11bb37e0d5687fb6cc48
Rizki Nugroho <54048440+sinugrepo@users.noreply.github.com>
2024-05-20 21:23:28 +0700

    Create run2.sh

---
+++ run2.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Update package lists
+apt update -y
+
+# Install Docker
+apt-get install docker.io -y
+
+# Install required packages
+apt-get install ca-certificates curl gnupg lsb-release -y
+
+# Pull Docker image
+docker pull nezha123/titan-edge:latest
+
+#create path
+mkdir ~/.titanedge
+
+# Run Docker containers nat
+docker run --network=host -d -v ~/.titanedge:/root/.titanedge --name titan1 nezha123/titan-edge:latest
+
+sleep 10
+
+# Bind containers to API
+docker exec titan1 titan-edge bind --hash=E0C400DE-17D4-4C1E-AEFA-9FCB2DA2ECB7 https://api-test1.container1.titannet.io/api/v2/device/binding
+
+# Set storage size for containers
+docker exec titan1 titan-edge config set --storage-size 58GB
+
+sleep 5
+
+cd /root/.titanedge
+
+sed -i 's#LocatorURL = "https://test-locator.titannet.io:5000/rpc/v0"#LocatorURL = "https://us-locator.titannet.io:5000/rpc/v0"#' config.toml
+
+sleep 1
+
+docker update --restart=always titan1
+
+sleep 2
+
+docker restart titan1
