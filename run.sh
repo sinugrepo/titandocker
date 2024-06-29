73d0353a03f8844aa8bac31bc1f0b4022ed9a7a5
Rizki Nugroho <54048440+sinugrepo@users.noreply.github.com>
2024-06-29 08:55:49 +0700

    Update run.sh
    Cassini Testnet Update

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 # Update package lists
 apt update -y
 
-ufw allow 1234
-
 sleep 1
+
 # Install Docker
 apt-get install docker.io -y
 
@@ -15,30 +14,42 @@ apt-get install ca-certificates curl gnupg lsb-release -y
 # Pull Docker image
 docker pull nezha123/titan-edge:latest
 
-#create path
-mkdir ~/.titanedge
+# Loop to create 18 containers
+for i in $(seq 1 18); do
+  # Create path for storing configurations
+  mkdir -p ~/.titanedge$i
 
-# Run Docker containers nat
-docker run --network=host -d -v ~/.titanedge:/root/.titanedge --name titan1 nezha123/titan-edge:latest
+  # Run Docker container with host network
+  docker run --network=host -d -v ~/.titanedge$i:/root/.titanedge --name titan$i nezha123/titan-edge:latest
 
-sleep 10
+  sleep 10
 
-# Bind containers to API
-docker exec titan1 titan-edge bind --hash=5D6040E3-AF64-47F1-9B42-D0FA4436C240 https://api-test1.container1.titannet.io/api/v2/device/binding
+  # Install Warp inside the container
+  docker exec titan$i bash -c "
+    apt update -y && 
+    apt install -y curl &&
+    curl https://pkg.cloudflareclient.com/pubkey.gpg | apt-key add - &&
+    echo 'deb http://pkg.cloudflareclient.com/ buster main' | tee /etc/apt/sources.list.d/cloudflare-client.list &&
+    apt update -y &&
+    apt install -y cloudflare-warp &&
+    warp-cli register &&
+    warp-cli connect &&
+    warp-cli status
+  "
 
-# Set storage size for containers
-docker exec titan1 titan-edge config set --storage-size 90GB
+  # Bind container to API
+  docker exec titan$i titan-edge bind --hash=D48160C8-5C42-4F78-B689-3FAE79A307FB https://api-test1.container1.titannet.io/api/v2/device/binding
 
-sleep 5
+  # Set storage size for the container
+  docker exec titan$i titan-edge config set --storage-size 50GB
 
-cd /root/.titanedge
+  sleep 5
 
-sed -i 's#LocatorURL = "https://test-locator.titannet.io:5000/rpc/v0"#LocatorURL = "https://sg-locator.titannet.io:5000/rpc/v0"#' config.toml
+  # Update container to always restart
+  docker update --restart=always titan$i
 
-sleep 1
+  sleep 2
 
-docker update --restart=always titan1
-
-sleep 2
-
-docker restart titan1
+  # Restart the container to apply changes
+  docker restart titan$i
+done
